91586669382e4a3853a2e6cb60b009038a415028
Nick Holcombe <nick@brightec.co.uk>
2015-11-03 15:14:55 +0000

    Added MainActivity to demo Rate The App

---
+++ app/src/main/java/uk/co/brightec/ratetheapp_android/MainActivity.java
@@ -0,0 +1,13 @@
+package uk.co.brightec.ratetheapp_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
