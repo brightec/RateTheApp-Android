ef0ed21ecf390047e84fe084146186ea89481bb3
Nick Holcombe <nick@brightec.co.uk>
2015-12-21 10:34:11 +0000

    Initial tidy-up of demo code, added an introduction explaining purpose of app. Combined default demo fragments into the first fragment and displayed the RateTheApp widgets in CardViews.

--- app/src/main/java/uk/co/brightec/ratetheapp_android/MainActivity.java
+++ app/src/main/java/uk/co/brightec/ratetheapp_android/MainActivity.java
@@ -14,8 +14,6 @@ import android.view.MenuItem;
 import android.view.View;
 
 import uk.co.brightec.ratetheapp_android.fragments.DefaultFragment;
-import uk.co.brightec.ratetheapp_android.fragments.Demo1Fragment;
-import uk.co.brightec.ratetheapp_android.fragments.Demo2Fragment;
 import uk.co.brightec.ratetheapp_android.fragments.Demo3Fragment;
 import uk.co.brightec.ratetheapp_android.fragments.Demo4Fragment;
 import uk.co.brightec.ratetheapp_android.fragments.Demo5Fragment;
@@ -117,10 +115,6 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.nav_default) {
             getSupportFragmentManager().beginTransaction().replace(R.id.demoHolder, DefaultFragment.newInstance()).commit();
-        } else if (id == R.id.nav_demo1) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.demoHolder, Demo1Fragment.newInstance()).commit();
-        } else if (id == R.id.nav_demo2) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.demoHolder, Demo2Fragment.newInstance()).commit();
         }
         else if (id == R.id.nav_demo3) {
             getSupportFragmentManager().beginTransaction().replace(R.id.demoHolder, Demo3Fragment.newInstance()).commit();
